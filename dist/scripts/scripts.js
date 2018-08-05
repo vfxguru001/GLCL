eba7dbaa75917f4034f78a26fc0ff8e73622fdc3
vfxguru001 <vfxguru001@mac.com>
2018-08-05 15:52:44 -0700

    First commit!

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -1,6 +1,23 @@
-"use strict";
+'use strict';
 
-var button = {};
+var fn = function fn() {
+    var minus = document.getElementsByClassName('minus');
+
+    for (var i = 0; i < minus.length; i++) {
+        var e = document.createElement('p');
+        e.innerHTML = '-';
+        minus[i].appendChild(e);
+    }
+
+    var plus = document.getElementsByClassName('plus');
+
+    for (var i = 0; i < plus.length; i++) {
+        var f = document.createElement('p');
+        f.innerHTML = '+';
+        plus[i].appendChild(f);
+    }
+};
+document.addEventListener('DOMContentLoaded', fn, false);
 'use strict';
 
 var list = [];

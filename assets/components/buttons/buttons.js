204529b8f3305c4cc29241a224933867e7345b1f
vfxguru001 <vfxguru001@mac.com>
2018-08-12 09:42:09 -0700

    updated buttons.js

--- assets/components/buttons/buttons.js
+++ assets/components/buttons/buttons.js
@@ -18,9 +18,9 @@ var fn = function() {
     var plus = document.getElementsByClassName('check');
 
     for (var i = 0; i < plus.length; i++) {
-        var f = document.createElement('p');
-        f.innerHTML = '+';
-        plus[i].appendChild(f);
+        var g = document.createElement('&#10004;');
+        g.innerHTML = '&#10004;';
+        plus[i].appendChild(g);
     }
 };
 document.addEventListener('DOMContentLoaded', fn, false);

425da1a861ca9bc2b5859226ec53535356045b82
Tim Aldridge <vfxguru001@mac.com>
2018-09-09 09:13:37 -0700

    Class prep - new branch

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -1,72 +1,30 @@
-'use strict';
+"use strict";
 
 var fn = function fn() {
-	// adding p tag for minus
-	var minus = document.getElementsByClassName('minus');
+  // adding p tag for minus
+  var minus = document.getElementsByClassName('minus');
 
-	for (var i = 0; i < minus.length; i++) {
-		var e = document.createElement('p');
-		e.innerHTML = '-';
-		minus[i].appendChild(e);
-	}
+  for (var i = 0; i < minus.length; i++) {
+    var e = document.createElement('p');
+    e.innerHTML = '-';
+    minus[i].appendChild(e);
+  } //adding p tag for plus
 
-	//adding p tag for plus
-	var plus = document.getElementsByClassName('plus');
-	for (var i = 0; i < plus.length; i++) {
-		var e = document.createElement('p');
-		e.innerHTML = '+';
-		plus[i].appendChild(e);
-	}
+
+  var plus = document.getElementsByClassName('plus');
+
+  for (var i = 0; i < plus.length; i++) {
+    var e = document.createElement('p');
+    e.innerHTML = '+';
+    plus[i].appendChild(e);
+  }
 };
 
 document.addEventListener('DOMContentLoaded', fn, false);
-'use strict';
+"use strict";
 
 var list = [];
+
 var myFunkyFunk = function myFunkyFunk() {
   console.log('the funk');
 };
-'use strict';
-
-var calendar = function calendar() {
-    var lastDay = function lastDay(year, month) {
-        return new Date(year, month, 0).getDate();
-    };
-    var firstDay = function firstDay(year, month) {
-        return new Date(year, month - 1, 1).getDay();
-    };
-
-    var daysOfTheWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
-    var calendarHead = document.getElementById('calendar-head');
-    var populateCalendarHead = function populateCalendarHead() {
-        for (var i = 0; i < daysOfTheWeek.length; i++) {
-            var day = document.createElement('p');
-            day.innerHTML = daysOfTheWeek[i];
-            calendarHead.appendChild(day);
-        }
-    };
-    populateCalendarHead();
-
-    var calendarBody = document.getElementById('calendar-body');
-    var populateCalendarBody = function populateCalendarBody() {
-        var daysInRow = 7;
-        var totalRows = 5;
-        // var totalDays = daysInRow * totalRows;
-        var currentRows = 0;
-
-        while (currentRows < totalRows) {
-            var calendarRow = document.createElement('div');
-            for (var i = 0; i < daysInRow; i++) {
-                console.log('within for loop');
-                var day = document.createElement('p');
-                day.innerHTML = i;
-                calendarRow.appendChild(day);
-            }
-            calendarBody.appendChild(calendarRow).className = "calendar-row";
-            currentRows++;
-        }
-    };
-    populateCalendarBody();
-};
-
-document.addEventListener('DOMContentLoaded', calendar, false);

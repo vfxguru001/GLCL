87967966c9393860fec1729711ec3b29cb6457e7
vfxguru001 <vfxguru001@mac.com>
2018-09-15 18:44:22 -0700

    should be clean now

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var concatCss = require('gulp-concat-css');
 let cleanCSS = require('gulp-clean-css');
 const babel = require('gulp-babel');
 
-gulp.task('default',['scripts','sass']);
+gulp.task('default', ['scripts', 'sass']);
 
 //script paths
 var jsFiles = 'assets/**/*.js',
@@ -15,26 +15,30 @@ var jsFiles = 'assets/**/*.js',
 
 gulp.task('scripts', function() {
     return gulp.src(jsFiles)
-      .pipe(babel({presets: ['env']}))
-      .pipe(concat('scripts.js'))
-      .pipe(gulp.dest(jsDest))
-      .pipe(rename('scripts.min.js'))
-      .pipe(uglify())
-      .pipe(gulp.dest(jsDest));
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(concat('scripts.js'))
+        .pipe(gulp.dest(jsDest))
+        .pipe(rename('scripts.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(jsDest));
 });
 
 //scss paths
 var scssFiles = 'assets/**/*.scss',
     cssDest = 'dist/css';
 
-gulp.task('sass', function(){
-  return gulp.src(scssFiles)
-    .pipe(sass()) // Using gulp-sass
-    .pipe(concatCss('styles.css'))
-    .pipe(gulp.dest(cssDest))
-    .pipe(rename('styles.min.css'))
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest(cssDest))
+gulp.task('sass', function() {
+    return gulp.src(scssFiles)
+        .pipe(sass()) // Using gulp-sass
+        .pipe(concatCss('styles.css'))
+        .pipe(gulp.dest(cssDest))
+        .pipe(rename('styles.min.css'))
+        .pipe(cleanCSS({
+            compatibility: 'ie8'
+        }))
+        .pipe(gulp.dest(cssDest))
 });
 
 //to understand how this works, watch Class 3 vid 52:00, "to create a single component with some modification"
